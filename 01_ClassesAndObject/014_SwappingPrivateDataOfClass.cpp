4cc45e18baf32bf72bee3b5ab2f60acf35b4a3c6
Sarif Iqbal <iqbalsarif.official@gmail.com>
2022-09-18 20:27:08 +0530

    Add files via upload

---
+++ 01_ClassesAndObject/014_SwappingPrivateDataOfClass.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class class_2;
+
+class class_1
+{
+    int value1;
+
+public:
+    void intdata(int a)
+    {
+        value1 = a;
+    }
+    void display(void)
+    {
+        cout << value1 << "\n";
+    }
+    friend void exchange(class_1 &, class_2 &);
+};
+class class_2
+{
+    int value2;
+
+public:
+    void intdata(int a)
+    {
+        value2 = a;
+    }
+    void display(void)
+    {
+        cout << value2 << "\n";
+    }
+    friend void exchange(class_1 &, class_2 &);
+};
+void exchange(class_1 &x, class_2 &y)
+{
+    int temp = x.value1;
+    x.value1 = y.value2;
+    y.value2 = temp;
+}
+int main()
+{
+    class_1 c1;
+    class_2 c2;
+    c1.intdata(100);
+    c2.intdata(100);
+    cout << "Values before exchange: " << endl;
+    c1.display();
+    c2.display();
+    exchange(c1, c2);
+    cout << "Values after exchange: " << endl;
+    c1.display();
+    c2.display();
+    return 0;
+}
